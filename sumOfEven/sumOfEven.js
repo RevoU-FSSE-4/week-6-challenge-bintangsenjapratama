6a464fc0bfbbd672c726ca92f163908622c59300
bintangsenjapratama <bintangsenjapratama@gmail.com>
2024-03-22 15:02:27 +0800

    feat: submit sumOfEvens function

--- sumOfEven/sumOfEven.js
+++ sumOfEven/sumOfEven.js
@@ -1,5 +1,16 @@
 function sumOfEvens(numbers) {
-  // Your code here
+  let sum = 0;
+
+  for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    const isEvenNumber = element % 2 === 0;
+
+    if (isEvenNumber) {
+      sum += element;
+    }
+  }
+
+  return sum;
 }
 
 module.exports = sumOfEvens;
