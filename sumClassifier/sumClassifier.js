224bfa05f4555a348ed5a9dcee37e2da8251c7fb
bintangsenjapratama <bintangsenjapratama@gmail.com>
2024-03-22 21:52:36 +0800

    feat: add sumClassifier code

--- sumClassifier/sumClassifier.js
+++ sumClassifier/sumClassifier.js
@@ -1,5 +1,17 @@
 function sumClassifier(numbers) {
-  // Your code here
+  let sum = 0;
+
+  for (let index = 0; index < numbers.length; index++) {
+    sum += numbers[index];
+  }
+
+  if (sum > 10) {
+    return "Large Sum";
+  } else if (sum === 10) {
+    return "Equal Sum";
+  } else {
+    return "Small Sum";
+  }
 }
 
 module.exports = sumClassifier;
