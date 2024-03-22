94ebba8987986ec74e2445116d91813b88866ce7
bintangsenjapratama <bintangsenjapratama@gmail.com>
2024-03-22 15:32:10 +0800

    feat: add sumOfSquares function

--- sumOfSquares/sumSquares.js
+++ sumOfSquares/sumSquares.js
@@ -1,5 +1,10 @@
 function sumOfSquares(num1, num2) {
-  // Your code here
+  let num1Square = num1 ** 2;
+  let num2Square = num2 ** 2;
+
+  sum = num1Square + num2Square;
+
+  return sum;
 }
 
 module.exports = sumOfSquares;
