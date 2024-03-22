17faf55433783ef9a2d9704f64a80d65a6b745b8
bintangsenjapratama <bintangsenjapratama@gmail.com>
2024-03-22 15:46:56 +0800

    fix: simplify the codes

--- sumOfSquares/sumSquares.js
+++ sumOfSquares/sumSquares.js
@@ -1,10 +1,5 @@
 function sumOfSquares(num1, num2) {
-  let num1Square = num1 ** 2;
-  let num2Square = num2 ** 2;
-
-  sum = num1Square + num2Square;
-
-  return sum;
+  return num1 ** 2 + num2 ** 2;
 }
 
 module.exports = sumOfSquares;
