dad7897a1b17fd1536a5f4577c07efa239bced10
bintangsenjapratama <bintangsenjapratama@gmail.com>
2024-03-22 21:32:57 +0800

    fix: rename to number

--- sumOfSquares/sumSquares.js
+++ sumOfSquares/sumSquares.js
@@ -1,6 +1,6 @@
 function sumOfSquares(num1, num2) {
-  function squares(num) {
-    return num ** 2;
+  function squares(number) {
+    return number ** 2;
   }
   return squares(num1) + squares(num2);
 }
